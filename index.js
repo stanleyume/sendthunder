9dcbe10f5e3998b3cbb70c1f216d8413d7b37e26
Stanley <coolstanlee@gmail.com>
2019-04-26 11:04:27 +0100

    Pug view engine + views

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+const path = require('path');
 const express = require('express');
 const app = express();
 
@@ -5,9 +6,13 @@ require('dotenv').config();
 
 const thunder = require('./thunder');
 
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
+
 app.get('/', (req, res) => {
   
-  res.send('Hello World');
+  // res.send('Hello World');
+  res.render('index.pug');
 });
 
 app.get('/email', (req, res) => {
