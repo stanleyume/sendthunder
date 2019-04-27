a1db409ba95863a71530c53ee40a4487fad9641d
Stanley <coolstanlee@gmail.com>
2019-04-27 23:46:04 +0100

    Body Parser

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
 const path = require('path');
 const express = require('express');
 const uuid = require('uuid/v1');
+const bodyParser = require('body-parser');
 const app = express();
 
 require('dotenv').config();
 
 const thunder = require('./thunder');
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
@@ -68,37 +72,44 @@ app.post('/ifttt/v1/test/setup', (req, res) => {
 
 app.post('/ifttt/v1/triggers/get_thunders', (req, res) => {
   res.header('Content-Type: application/json; charset=utf-8');
-  res.status(200).json({
-    data: [
-      {
-        "recipient": "@StreetArt",
-        "thunder_name": "Blue thunder",
-        "created_at": new Date().toISOString(),
-        "meta": {
-          "id": uuid(),
-          "timestamp": Date.now() - 80
-        }
-      },
-      {
-        "recipient": "@Technology",
-        "thunder_name": "Red thunder",
-        "created_at": new Date().toISOString(),
-        "meta": {
-          "id": uuid(),
-          "timestamp": Date.now() - 100
-        }
-      },
-      {
-        "recipient": "@Someone",
-        "thunder_name": "Green thunder",
-        "created_at": new Date().toISOString(),
-        "meta": {
-          "id": uuid(),
-          "timestamp": Date.now() - 200
-        }
-      }
 
-    ]
+  let items = [
+    {
+      "recipient": "@StreetArt",
+      "thunder_name": "Blue thunder",
+      "created_at": new Date().toISOString(),
+      "meta": {
+        "id": uuid(),
+        "timestamp": parseInt((new Date('2019.04.10').getTime() / 1000).toFixed(0))
+      }
+    },
+    {
+      "recipient": "@Technology",
+      "thunder_name": "Red thunder",
+      "created_at": new Date().toISOString(),
+      "meta": {
+        "id": uuid(),
+        "timestamp": parseInt((new Date('2019.04.05').getTime() / 1000).toFixed(0))
+      }
+    },
+    {
+      "recipient": "@Someone",
+      "thunder_name": "Green thunder",
+      "created_at": new Date().toISOString(),
+      "meta": {
+        "id": uuid(),
+        "timestamp": parseInt((new Date('2019.04.01').getTime() / 1000).toFixed(0))
+      }
+    }
+
+  ];
+
+  if (req.body.limit) {
+    items = items.slice(0, req.body.limit);
+  }
+  
+  res.status(200).json({
+    data: items
   });
 });
 
