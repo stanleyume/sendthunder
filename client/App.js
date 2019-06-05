fd06e2c42323ab69a35f0608721fb17a798ed8ec
Stanley <coolstanlee@gmail.com>
2019-06-05 20:13:03 +0100

    UI changes

--- client/App.js
+++ client/App.js
@@ -53,42 +53,68 @@ class App extends Component {
 
   render(){  
 
-    return (<div style={{ width:'60%', textAlign: 'center', margin: '0 auto' }}>
+    return (<div>
 
-    <h2>Send Thunder {process.env.API_HOST}</h2>
+    <h1 className="title">Send Thunder</h1>
+    <h3 className="sub-title">Anonymously send thunder to any Twitter user of your choice</h3>
   
     <form onSubmit={this.handleSubmit.bind(this)} method="POST">
 
-      <FormGroup label="Select Thunder">
+      {/* <FormGroup label="Select Thunder">
         <HTMLSelect large fill name="thunder" options={this.state.thunders.map(a=>(a.name))}>
-          {/* { this.state.thunders.map(thunder => `<option>${thunder.name}</option>`) } */}
+          
         </HTMLSelect>
       </FormGroup>
 
-      <FormGroup label="Recepient's Twitter"
-          >
+      <FormGroup label="Recepient's Twitter">
         <ControlGroup fill={true} vertical={false}>
             <Button style={{ maxWidth: '50px'  }}><AtSign/></Button>
             <InputGroup placeholder="umestanley" name="recipient" large/>
         </ControlGroup>
       </FormGroup>
+
+      <br/>
+
+      <Button intent="success" large type="submit">Fire</Button> */}
+
+      <select name="thunder" style={{ marginBottom: '15px', marginTop: '25px' }}>
+        { this.state.thunders.map((thunder, i) => <option value={thunder.name} key={i}>{thunder.name}</option>) }
+      </select>
+
+      <div style={{ display: 'flex' }}>
+        <input value="@" readOnly style={{ width:'10%', minWidth: '50px', textAlign: 'center', borderRadius: '7px 0 0 7px', borderRight: 'none' }}/>
+        <input name="recipient" placeholder="Twitter handle" autocomplete="off" style={{ borderRadius: '0 7px 7px 0' }}/>
+      </div>
+
       
       <br/>
 
-      <Button intent="success" large type="submit">Fire</Button>
+      <button type="submit" className="submit-btn">⚡ Fire</button>
 
     </form>
 
     <br/>
     
-    <form onSubmit={this.createThunder.bind(this)}>
+    {/* <form onSubmit={this.createThunder.bind(this)}>
       <InputGroup placeholder="Thunder title" name="name" large required/>
       <Button intent="success" large type="submit">Save</Button>
-    </form>
+    </form> */}
 
-    <ul>
+    {/* <ul>
       {this.state.orders.map((order, index) => { return <li key={index}>{order.thunder_name} > {order.recipient}</li>})}
-    </ul>
+    </ul> */}
+
+    <table>
+      <thead>
+        <th>Thunder</th>
+        <th>Recipient</th>
+      </thead>
+      <tbody>
+        {this.state.orders.map((order, index) => { return <tr key={index}><td>{order.thunder_name}</td><td>{order.recipient}</td></tr>})}
+      </tbody>
+    </table>
+
+    <div className="footer">&copy; All Rice Re-Served 🍚</div>
   
   </div>);
   }
